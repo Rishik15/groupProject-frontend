ea8c06c89eaa9e5039205f5df05982052add99ae
Rishik <rishikreddy.yesgari@gmail.com>
2026-03-21 01:33:24 -0400

    Change route in navbar

--- src/components/LandingPage/StickyHeader.tsx
+++ src/components/LandingPage/StickyHeader.tsx
@@ -9,12 +9,12 @@ const StickyHeader = () => {
   const navigate = useNavigate();
 
   // Current working routes.
-  const landingRoute = "/";
+  const signin = "/signin";
   const registerRoute = "/register";
 
   return (
     <header className="sticky top-0 h-14 z-100 bg-[#f8f8fb] border-b border-[#E6E6EE] shadow-sm">
-      <div className="max-w-7xl mx-auto h-16 px-14 flex items-center justify-between">
+      <div className="max-w-7xl mx-auto h-14 px-16 flex items-center justify-between">
         <Link to="/" className="flex items-center gap-2">
           <div className="text-white text-[12px] font-semibold w-7 h-7 flex items-center justify-center bg-[#5B5EF4] rounded-lg p-0">
             βF
@@ -31,7 +31,7 @@ const StickyHeader = () => {
           <TemplateButton
             variant="ghost"
             className="text-[14px]"
-            onClick={() => navigate(landingRoute)}
+            onClick={() => navigate(signin)}
           >
             Sign in
           </TemplateButton>
