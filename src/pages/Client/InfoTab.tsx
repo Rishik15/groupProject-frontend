e16947f4e8ee39e7d6293c1fe7e31af9d8823d19
Albi Zhaku <albizhaku15@gmail.com>
2026-04-07 23:17:35 -0400

    Fixed selector

--- src/pages/Client/InfoTab.tsx
+++ src/pages/Client/InfoTab.tsx
@@ -2,40 +2,40 @@ import { Card, Input, Label, Link, TextArea } from "@heroui/react";
 
 export function InfoTab() {
     return (
-        <Card className="w-auto">
-            <Card.Footer className="flex flex-col items-start">
-                <div className="flex flex-col">
+        <Card className="w-auto rounded-xl border-1 border-[#E8E8EF]">
+            <Card.Footer className="flex flex-col items-start gap-4 pt-2 pl-2 pr-2">
+                <div className="flex flex-col w-full gap-2">
                     <Label>Full Name</Label>
-                    <Input></Input>
+                    <Input className="bg-gray-100 rounded-md" />
                 </div>
-                <div className="flex flex-col">
+                <div className="flex flex-col w-full gap-2">
                     <Label>Email</Label>
-                    <Input/>
+                    <Input className="bg-gray-100 rounded-md" />
                 </div>
 
-                <div className="grid grid-cols-3 gap-4">
-                    <div className="flex flex-col">
+                <div className="grid grid-cols-3 gap-4 w-full gap-2">
+                    <div className="flex flex-col gap-2">
                         <Label>Age</Label>
-                        <Input />
+                        <Input className="bg-gray-100 rounded-md" />
                     </div>
 
-                    <div className="flex flex-col">
+                    <div className="flex flex-col gap-2">
                         <Label>Weight</Label>
-                        <Input />
+                        <Input className="bg-gray-100 rounded-md" />
                     </div>
 
-                    <div className="flex flex-col">
+                    <div className="flex flex-col gap-2">
                         <Label>Height</Label>
-                        <Input />
+                        <Input className="bg-gray-100 rounded-md" />
                     </div>
                 </div>
-                <div className="flex flex-col">
-                    <Label>Goals</Label>
-                    <Input/>
+                <div className="flex flex-col w-full gap-2">
+                    <Label >Goals</Label>
+                    <Input className="bg-gray-100 rounded-md" />
                 </div>
-                <div className="flex flex-col">
+                <div className="flex flex-col w-full gap-2">
                     <Label>Bio</Label>
-                    <TextArea></TextArea>
+                    <TextArea className="bg-gray-100"/>
                 </div>
             </Card.Footer>
         </Card>
