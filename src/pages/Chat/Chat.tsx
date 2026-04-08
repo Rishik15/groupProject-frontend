49f3b9f71526c35b579dcb6155169f58a946a979
Rishik <rishikreddy.yesgari@gmail.com>
2026-04-08 17:33:12 -0400

    Add Basic UI of chat page with fake data

--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import ChatMessages from "../../components/chat/ChatMessage";
 import ChatSidebar from "../../components/chat/ChatSidebar";
+import ChatWindow from "../../components/chat/ChatWindow";
 
 const users = [
   {
@@ -24,11 +26,49 @@ const users = [
   },
 ];
 
+type Message = {
+  id: number;
+  text: string;
+  timestamp: string;
+  type: "sent" | "received";
+};
+
+const messages: Message[] = [
+  {
+    id: 1,
+    text: "Hey bro",
+    timestamp: "10:30 AM",
+    type: "received",
+  },
+  {
+    id: 2,
+    text: "What's up?",
+    timestamp: "10:31 AM",
+    type: "received",
+  },
+  {
+    id: 3,
+    text: "All good, working out",
+    timestamp: "10:32 AM",
+    type: "sent",
+  },
+];
+
 const Chat = () => {
+  const [selectedUser, setSelectedUser] = useState<any>(null);
+
   return (
-    <main className="w-full">
-      <div className="flex max-w-7xl mx-auto px-6">
-        <ChatSidebar users={users} />
+    <main className="mt-16 flex items-center justify-center px-36">
+      <div className="flex max-w-5xl w-full">
+        <ChatSidebar users={users} onSelectUser={setSelectedUser} />
+
+        <div className="flex-1">
+          {!selectedUser ? (
+            <ChatMessages />
+          ) : (
+            <ChatWindow user={selectedUser} messages={messages} />
+          )}
+        </div>
       </div>
     </main>
   );
