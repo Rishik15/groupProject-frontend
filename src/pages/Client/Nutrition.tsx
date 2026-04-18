d210e392b4d90069548c82ff36a8317aa8f97a21
grugg1233 <gma35@njit.edu>
2026-04-18 03:30:24 -0400

    merged all

--- src/pages/Client/Nutrition.tsx
+++ src/pages/Client/Nutrition.tsx
@@ -1,34 +1,4 @@
-<<<<<<< HEAD
-import { useState } from "react";
-import MealLoggingModal from "../../components/MealLoggingModal/Modal";
-import NutritionHeader from "../../components/NutritionPage/NutritionHeader";
-import NutritionTabs from "../../components/NutritionPage/Tabs";
 
-const Nutrition = () => {
-    const [isMealModalOpen, setIsMealModalOpen] = useState(false);
-
-    return (
-        <section className="bg-[#F7F7FB]">
-            <NutritionHeader onLogMeal={() => setIsMealModalOpen(true)} />
-
-            <div className="px-36 py-6">
-                <div className="flex items-left">
-                    <NutritionTabs />
-                </div>
-            </div>
-
-            <MealLoggingModal
-                isOpen={isMealModalOpen}
-                onOpenChange={setIsMealModalOpen}
-                onSuccess={() => {
-                    console.log("Meal logged successfully");
-                }}
-            />
-        </section>
-    );
-};
-
-=======
 import { useState } from "react";
 import MealLoggingModal from "../../components/MealLoggingModal/Modal";
 import NutritionHeader from "../../components/NutritionPage/NutritionHeader";
@@ -59,5 +29,4 @@ const Nutrition = () => {
     );
 };
 
->>>>>>> 22064e8ba5687c54f20c66bf425f607dd431124b
 export default Nutrition;
