7c63b8bd233ec9b8c66c66cfcdd7472a573e9dda
Rishik <rishikreddy.yesgari@gmail.com>
2026-04-18 21:31:06 -0400

    Made ui changes to wellness survey

--- src/components/Client/WellnessCheck/WellnessButton.tsx
+++ src/components/Client/WellnessCheck/WellnessButton.tsx
@@ -1,14 +1,16 @@
 import { Button } from "@heroui/react";
 
 const WellnessButton = () => {
-
-    return (
-        <Button type="button" className="h-8 px-3 rounded-xl bg-gray-100 text-black hover:bg-[#5B5EF4] hover:text-white">
-            <div className="flex items-center gap-2">
-                <div className="text-[12px]">Mental Wellness</div>
-            </div>
-        </Button>
-    );
-}
+  return (
+    <Button
+      type="button"
+      className="h-8 px-3 rounded-xl bg-gray-100 text-black hover:bg-[#5B5EF4] hover:text-white"
+    >
+      <div className="flex items-center gap-2">
+        <div className="text-[12px]">Mental Wellness</div>
+      </div>
+    </Button>
+  );
+};
 
 export default WellnessButton;
