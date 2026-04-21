1609ce1c3ec7d988bc83757fa53ecc981f191a30
Kevin Drake <kld33@njit.edu>
2026-04-21 15:06:36 -0400

    Fix

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -12,8 +12,8 @@ const AdminLayout = () => {
                 parent="/admin"
                 name="Admin"
                 email="admin@email.com"
-                notifications={[]}
-                count={0}
+                notifications={[]} //This needs fixing
+                count={0} //Same deal here
             />
 
             <div className="pt-14">
