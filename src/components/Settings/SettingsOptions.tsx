49d89f62e214f1790d0bd428ebec12bedb7a280f
Albi Zhaku <albizhaku15@gmail.com>
2026-04-21 22:03:58 -0400

    Added a help & support modal

--- src/components/Settings/SettingsOptions.tsx
+++ src/components/Settings/SettingsOptions.tsx
@@ -1,4 +1,4 @@
-import {  Button } from "@heroui/react";
+import { Button } from "@heroui/react";
 import {
   ChevronRight,
   LogOut,
@@ -6,6 +6,9 @@ import {
 import { AccountDeletion } from "./AccountDeletion";
 import type { LucideIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { HelpCircle } from "lucide-react";
+import HelpModal from "./HelpModal";
 
 type OptionItem = {
   label: string;
@@ -20,6 +23,7 @@ type Props = {
 
 const SettingOptions = ({ options, onLogout }: Props) => {
   const navigate = useNavigate();
+  const [openModal, setOpenModal] = useState(false);
 
   return (
     <div className="flex flex-col gap-10 w-165">
@@ -31,9 +35,8 @@ const SettingOptions = ({ options, onLogout }: Props) => {
             <Button
               key={option.label}
               onClick={() => navigate(option.route)}
-              className={`w-full h-[50px] px-6 flex items-center justify-between bg-transparent hover:bg-[#f9fafb] rounded-none ${
-                index !== options.length - 1 ? "border-b border-[#e5e7eb]" : ""
-              }`}
+              className={`w-full h-[50px] px-6 flex items-center justify-between bg-transparent hover:bg-[#f9fafb] rounded-none ${index !== options.length - 1 ? "border-b border-[#e5e7eb]" : ""
+                }`}
             >
               <div className="flex items-center gap-4">
                 <Icon className="w-5 h-5 text-gray-500" />
@@ -43,21 +46,32 @@ const SettingOptions = ({ options, onLogout }: Props) => {
             </Button>
           );
         })}
+        <Button
+          onClick={() => setOpenModal(true)}
+          className="w-full h-[50px] px-6 flex items-center justify-between bg-transparent hover:bg-[#f9fafb] rounded-none border-t border-[#e5e7eb]"
+        >
+          <div className="flex items-center gap-4">
+            <HelpCircle className="w-5 h-5 text-gray-500" />
+            <span className="text-base text-black">Help & Support</span>
+          </div>
+          <ChevronRight className="w-5 h-5 text-gray-400" />
+        </Button>
+        <HelpModal setIsOpen={setOpenModal} isOpen={openModal}/>
       </div>
 
-         <div className="flex gap-3 items-center justify-center">
-      <Button
-        className="w-f rounded-md bg-transparent border border-gray-300"
-        onClick={() => {
-          onLogout();
-          navigate("/");
-        }}
-      >
-        <LogOut className="text-black" />
-        <p className="text-black">Sign Out</p>
-      </Button>
+      <div className="flex gap-3 items-center justify-center">
+        <Button
+          className="w-f rounded-md bg-transparent border border-gray-300"
+          onClick={() => {
+            onLogout();
+            navigate("/");
+          }}
+        >
+          <LogOut className="text-black" />
+          <p className="text-black">Sign Out</p>
+        </Button>
 
-      <AccountDeletion />
+        <AccountDeletion />
       </div>
     </div>
   );
