bd8161f5218b9eb99f9462fb769c8462d79dd865
Rishik <rishikreddy.yesgari@gmail.com>
2026-04-22 16:32:12 -0400

    Add admin role

--- src/utils/auth/ProtectedRoute.tsx
+++ src/utils/auth/ProtectedRoute.tsx
@@ -35,9 +35,15 @@ const ProtectedRoute = ({
       allowedRoles &&
       !allowedRoles.some((r) => roles.includes(r))
     ) {
-      if (roles.includes("coach")) navigate("/coach", { replace: true });
-      else if (roles.includes("client")) navigate("/client", { replace: true });
-      else navigate("/", { replace: true });
+      if (roles.includes("admin")) {
+        navigate("/admin", { replace: true });
+      } else if (roles.includes("coach")) {
+        navigate("/coach", { replace: true });
+      } else if (roles.includes("client")) {
+        navigate("/client", { replace: true });
+      } else {
+        navigate("/", { replace: true });
+      }
     }
   }, [status, roles, allowedRoles, hasCheckedAuth, navigate]);
 
