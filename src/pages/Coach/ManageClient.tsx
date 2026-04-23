eea0f35ffe292553a07d0f2206d8a51436dbc8a6
Rishik <rishikreddy.yesgari@gmail.com>
2026-04-23 16:54:51 -0400

    Made UI changes

--- src/pages/Coach/ManageClient.tsx
+++ src/pages/Coach/ManageClient.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import ManageClientModal from "../../components/Coach/ManageClients/ManageClientModal";
 import { getClients } from "../../services/ManageClients/getClients";
+import { Card, Avatar } from "@heroui/react";
 
 type Client = {
   contract_id: number;
@@ -25,21 +26,44 @@ export default function ManageClients() {
   }, []);
 
   return (
-    <div className="py-8 px-38">
-      <h1 className="text-2xl font-semibold mb-2">Manage Clients</h1>
-      <h2 className="text-[14px] mb-6">
-        Manage your clients dashboard, nutrition and workout plan
-      </h2>
+    <div className="min-h-screen bg-[#F5F7FB] px-38 py-12">
+      <div className="mb-8">
+        <h1 className="text-3xl font-semibold text-[#111]">Manage Clients</h1>
+        <p className="text-[14px] text-gray-500 mt-2">
+          View and manage client dashboards, nutrition and workouts
+        </p>
+      </div>
 
-      <div className="grid grid-cols-4 gap-4">
+      <div className="grid grid-cols-4 gap-8">
         {clients.map((c) => (
-          <div
+          <Card
             key={c.contract_id}
             onClick={() => setSelectedContract(c.contract_id)}
-            className="p-5 border rounded-xl cursor-pointer hover:shadow transition"
+            className="
+              cursor-pointer
+              rounded-2xl
+              px-5 py-4
+              border border-[#E5E7EB]
+              bg-white
+              transition-all
+              hover:shadow-xl
+              hover:-translate-y-1
+              hover:bg-indigo-50
+              group
+            "
           >
-            {c.name}
-          </div>
+            <div className="flex items-center gap-3">
+              <Avatar className="w-8 h-8">
+                <Avatar.Image src="https://heroui-assets.nyc3.cdn.digitaloceanspaces.com/avatars/blue.jpg" />
+              </Avatar>
+
+              <div className="flex flex-col">
+                <span className="text-[15px] font-semibold text-[#111] group-hover:text-indigo-600 transition">
+                  {c.name}
+                </span>
+              </div>
+            </div>
+          </Card>
         ))}
       </div>
 
