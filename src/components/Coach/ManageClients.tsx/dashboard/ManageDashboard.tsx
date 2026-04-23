f148303dd46c8c82ff640be969294034c7316f6b
Rishik <rishikreddy.yesgari@gmail.com>
2026-04-23 11:49:48 -0400

    Add dashboard for client to look at

--- src/components/Coach/ManageClients.tsx/dashboard/ManageDashboard.tsx
+++
@@ -1,9 +0,0 @@
-const Dashboard = ({ contractId }: { contractId: number }) => {
-  return (
-    <section>
-      <div>Hello</div>
-    </section>
-  );
-};
-
-export default Dashboard;
--- src/pages/Coach/ManageClient.tsx
+++ src/pages/Coach/ManageClient.tsx
@@ -1,24 +1,42 @@
-import { useState } from "react";
-import ManageClientModal from "../../components/Coach/ManageClients.tsx/ManageClientModal";
+import { useState, useEffect } from "react";
+import ManageClientModal from "../../components/Coach/ManageClients/ManageClientModal";
+import { getClients } from "../../services/ManageClients/getClients";
+
+type Client = {
+  contract_id: number;
+  name: string;
+};
 
 export default function ManageClients() {
+  const [clients, setClients] = useState<Client[]>([]);
   const [selectedContract, setSelectedContract] = useState<number | null>(null);
 
-  const clients = [
-    { name: "Alex Taylor", contract_id: 1 },
-    { name: "John Doe", contract_id: 2 },
-  ];
+  useEffect(() => {
+    const fetchClients = async () => {
+      try {
+        const data = await getClients();
+        setClients(data || []);
+      } catch (err) {
+        console.error("failed to fetch clients", err);
+      }
+    };
+
+    fetchClients();
+  }, []);
 
   return (
-    <div className="p-8">
-      <h1 className="text-2xl font-semibold mb-6">Manage Clients</h1>
+    <div className="py-8 px-38">
+      <h1 className="text-2xl font-semibold mb-2">Manage Clients</h1>
+      <h2 className="text-[14px] mb-6">
+        Manage your clients dashboard, nutrition and workout plan
+      </h2>
 
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-4 gap-4">
         {clients.map((c) => (
           <div
             key={c.contract_id}
             onClick={() => setSelectedContract(c.contract_id)}
-            className="p-5 border rounded-xl cursor-pointer hover:shadow"
+            className="p-5 border rounded-xl cursor-pointer hover:shadow transition"
           >
             {c.name}
           </div>
