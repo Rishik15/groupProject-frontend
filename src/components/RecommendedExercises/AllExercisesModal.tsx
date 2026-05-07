83d093ca4a2a05695cf2dc8a441d0c38403e914b
Rishik <rishikreddy.yesgari@gmail.com>
2026-05-07 16:09:07 -0400

    Add ui changes

--- src/components/RecommendedExercises/AllExercisesModal.tsx
+++ src/components/RecommendedExercises/AllExercisesModal.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 const AllExercisesModal = ({ plans, isOpen, setIsOpen }: Props) => {
   return (
-    <Modal.Backdrop isOpen={isOpen} onOpenChange={setIsOpen}>
+    <Modal.Backdrop isOpen={isOpen} onOpenChange={setIsOpen} variant="blur">
       <Modal.Container>
         <Modal.Dialog className="sm:max-w-[800px] h-1000">
           <Modal.CloseTrigger />
